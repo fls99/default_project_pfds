f5aa8cc21954eff13008326cdf8f9979334d85aa
Felix Steppack <felix.steppack@googlemail.com>
2024-11-05 12:40:45 +0100

    janga

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", text="testing")
+    return render_template("login.html", text=True)
 
 @auth.route('/logout')
 def logout():
